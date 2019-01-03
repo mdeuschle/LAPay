1f498005c3cbe88a9f80e66c2409cfc01dc122f6
Matthew Deuschle <mdeuschle@gmail.com>
2019-01-02 20:50:01 -0800

    update url components

--- LAPay/Networking/Services/WebService.swift
+++ LAPay/Networking/Services/WebService.swift
@@ -13,7 +13,7 @@ struct WebService: DataSource {
     private init() {}
     
     private func urlString() -> String {
-        let base = "https://controllerdata.lacity.org/resource/969q-4gr3.json?$limit=100000&year="
+        let base = "https://controllerdata.lacity.org/resource/969q-4gr3.json?$where=total_payments>80000.00&$limit=50000&year="
         let date = Date()
         let userDefaults = UserDefaults.standard
         userDefaults.set(date, forKey: "date")
@@ -24,8 +24,26 @@ struct WebService: DataSource {
         return base + "\(previousYearInt)"
     }
     
+    private func components(from urlString: String) -> URLComponents? {
+        guard let path = urlString.components(separatedBy: "?").first else { return nil }
+        var components = URLComponents(string: path)
+        if let queryString = urlString.components(separatedBy: "?").last {
+            components?.queryItems = []
+            let queryItems = queryString.components(separatedBy: "&")
+            for queryItem in queryItems {
+                guard let name = queryItem.components(separatedBy: "=").first,
+                    let value = queryItem.components(separatedBy: "=").last else {
+                        continue
+                }
+                components?.queryItems?.append(URLQueryItem(name: name, value: value))
+            }
+        }
+        return components
+    }
+    
     func dataSource(completion: @escaping (Response<Data>) -> Void) {
-        guard let url = URL(string: urlString()) else {
+        let components = self.components(from: urlString())
+        guard let url = components?.url else {
             fatalError("Could not compose a valid URL in \(#file).\(#function)")}
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
             let response: Response<Data>
