1088da9a7f68129b987e647595a73524494accc6
Matthew Deuschle <mdeuschle@gmail.com>
2019-01-13 22:21:12 -0800

    add token

---
+++ LAPay/Networking/Services/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  LAPay
+//
+//  Created by Matt Deuschle on 1/13/19.
+//  Copyright © 2019 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+
+enum Token {
+    static let string = "LFuu36jqve2Td9BWBffSS1iJm"
+}
