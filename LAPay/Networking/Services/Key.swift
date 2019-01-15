0dc5da3e664721805d8060f031c9685d824f5bd2
Matthew Deuschle <mdeuschle@gmail.com>
2019-01-14 21:26:56 -0800

    add key

---
+++ LAPay/Networking/Services/Key.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  LAPay
+//
+//  Created by Matt Deuschle on 1/14/19.
+//  Copyright © 2019 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+
+enum Key {
+    static let string = "LFuu36jqve2Td9BWBffSS1iJm"
+}
